d0247e08ddc666c4f5b2be0016bc7fdc4c56d0e6
Stella Talcott <stalcott2027@kehillahstudent.org>
2025-04-30 12:10:20 -0700

    my answer

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import json
 
 # read `expenses.json`
+with open("expenses.json", "r") as f:
+    expenses = json.load(f)
 
 # get and print total "price" for all expenses at the "pet store"
+pet_store = expenses["pet store"]
+
+cost = 0
+for purchase in pet_store:
+    cost += purchase["price"]
+
+print(f"the total price from the pet_store is ${cost}")
